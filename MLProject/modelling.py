bcdbc3ddec96f9c5a840057623bac65d0f8134f2
Xebec51 <rinaldi.ruslan51@gmail.com>
2026-05-17 01:10:52 +0800

    fix: hardcode python 3.12.7 in model signature to prevent docker build crash
    - Menerapkan custom_env pada mlflow.sklearn.log_model untuk memaksa versi Python 3.12.7.
    
    - Memutus rantai error get-pip.py dengan mencegah MLflow men-generate Dockerfile berbasis Python 3.9.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,6 +3,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
+import sklearn
+
+# Dilarang menggunakan mlflow.set_experiment() di sini
 
 with mlflow.start_run():
     # 1. Load Data
@@ -11,7 +14,7 @@ with mlflow.start_run():
     y_train = pd.read_csv("dataset_processed/y_train.csv").squeeze()
     y_test = pd.read_csv("dataset_processed/y_test.csv").squeeze()
 
-    # 2. Train Model (Gunakan parameter standar saja untuk CI agar cepat)
+    # 2. Train Model
     model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
     model.fit(X_train, y_train)
 
@@ -20,10 +23,28 @@ with mlflow.start_run():
     accuracy = accuracy_score(y_test, y_pred)
     mlflow.log_metric("accuracy", accuracy)
 
-    # 4. Log Model (INI YANG PALING KRUSIAL AGAR DOCKER BISA BUILD)
+    # 4. MEMAKSA LINGKUNGAN PYTHON 3.12.7 SECARA HARDCODE (SOLUSI DOCKER BUILD ERROR)
+    custom_env = {
+        "channels": ["conda-forge"],
+        "dependencies": [
+            "python=3.12.7",  # Kunci utamanya ada di sini!
+            "pip",
+            {
+                "pip": [
+                    "mlflow==2.19.0",
+                    f"scikit-learn=={sklearn.__version__}",
+                    "pandas"
+                ]
+            },
+        ],
+        "name": "mlflow-env"
+    }
+
+    # 5. Log Model dengan menyuntikkan Custom Environment
     mlflow.sklearn.log_model(
         sk_model=model,
-        artifact_path="model"
+        artifact_path="model",
+        conda_env=custom_env
     )
 
-    print(f"Model CI berhasil dilatih dengan akurasi {accuracy:.4f} dan disimpan ke mlruns!")
+    print(f"Model CI berhasil dilatih dengan akurasi {accuracy:.4f} dan Environment Python 3.12.7 terkunci!")
